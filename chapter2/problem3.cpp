6c161d2dc08e8a3c47097469ecc41d80eebc0192
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-01-20 09:32:58 +0100

    updated code to make more readable

--- chapter2/problem3.cpp
+++ chapter2/problem3.cpp
@@ -3,54 +3,29 @@
 using namespace std;
 
 // STEPS:
-// Take a number 0-9, double it and return the sum of its digits
-// convert char to int
-// read number from user of fixed length and sum the the digitis with doubling
-//
+// Read string from user input (string makes it easier to loop over value)
+// Loop backwards over characters in string
+// Convert character to int
+// Double int and return the sum of its digits (e.g. 8*2 = 16 = 7 (1+6))
+// Loop over string and detect every other character
+// Combine above -> Loop over string and sum digits, double every other one
+// If total sum is divisible by 10, number is valid
 
-// STEP 1: double number and return sum of its digits
+string getUsersNum() {
+    string ccNum;
+    cout << "Type your credit card number: ";
+    cin >> ccNum;
+    return ccNum;
+}
+
 int doubleAndSum(int num) {
     int doubled = num * 2;
     if (doubled < 10) return doubled;
     return (1 + (doubled - 10));
 }
 
-// STEP 2: convert char to int
 int ctoi(char character) { return character - '0'; }
 
-// STEP 3: Sum each digit of a number
-/*int sumDigits(string num) {
-    int sum = 0;
-    for (size_t i = 0; i < num.length(); i++) {
-        sum += ctoi(num[i]);
-    }
-    return sum;
-}*/
-
-// STEP 4: sum digits, but double every other value and sum the result's digits
-/*int sumDigits(string num) {
-    int sum = 0;
-    for (size_t i = 0; i < num.length(); i++) {
-        if (i % 2 == 1) {
-            int digit = ctoi(num[i]);
-            sum += doubleAndSum(digit);
-        } else {
-            sum += ctoi(num[i]);
-        }
-    }
-    return sum;
-}*/
-
-// STEP 5: Loop backwards
-void loopBackwards(string num) {
-    for (size_t i = num.length(); i--;) {
-        cout << num[i];
-    }
-}
-
-// STEP 6: Combine above to loop backwards and double every other digit
-// use a counter to make sure every other digit is doubled regardless of string
-// length (i.e. it doesn't matter if string length is even or odd)
 int sumDigits(string num) {
     int sum = 0;
     int evenTracker = 1;
@@ -66,13 +41,10 @@ int sumDigits(string num) {
     return sum;
 }
 
-// STEP 7: Check if sum is divisible by 10
 bool ccNumIsValid(int num) { return num % 10 == 0; }
 
 int main() {
-    string ccNum;
-    cout << "Type your credit card number: ";
-    cin >> ccNum;
+    string ccNum = getUsersNum();
     int sum = sumDigits(ccNum);
     if (ccNumIsValid(sum)) {
         cout << "Credit card number is valid";
@@ -81,5 +53,3 @@ int main() {
     }
     return 0;
 }
-
-// 1 2 3 4 5 6 => 1 4 3 8 5 3 => 24
