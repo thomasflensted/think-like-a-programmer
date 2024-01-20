86d7b1724706bdbcdcef7f14eddb3d5946c2efc1
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-01-20 10:25:17 +0100

    created file and wrote boilerplate

---
+++ chapter2/problem4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// STEPS
+// Read character by character until end of array/end-of-line
+// Convert a series of characters representing a number to an int
+// Convert an integer 1-26 into upper- or lowercase letter
+// Convert an integer 1-8 to a punctuation symbol
+// Track the decoded message
+
+int main() { return 0; }
